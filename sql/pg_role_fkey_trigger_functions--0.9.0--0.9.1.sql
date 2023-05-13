838ed1283b0f7083a42673b57018364ed850daec
Rowan <rowan@bigsmoke.us>
2023-05-13 15:06:26 +0100

    Make true to the claim that `superuser = false`

--- sql/pg_role_fkey_trigger_functions--0.9.0--0.9.1.sql
+++ sql/pg_role_fkey_trigger_functions--0.9.0--0.9.1.sql
@@ -3,11 +3,33 @@
 
 --------------------------------------------------------------------------------------------------------------
 
+-- This was originally an unconditional `ALTER DATABASE` statement, which disregarded the facts that the
+-- `.control` file of this extension states that this extension should be installable for non-superusers.
 do $$
+declare
+    _ddl_cmd_to_set_pg_role_fkey_trigger_functions_url text;
 begin
-    execute 'ALTER DATABASE ' || current_database()
-        || ' SET pg_role_fkey_trigger_functions.readme_urls TO '
-        || quote_literal('https://github.com/bigsmoke/pg_role_fkey_trigger_functions/blob/master/README.md');
+    _ddl_cmd_to_set_pg_role_fkey_trigger_functions_url := format(
+        'ALTER DATABASE %I SET pg_role_fkey_trigger_functions.readme_url = %L'
+        ,current_database()
+        ,'https://github.com/bigsmoke/pg_role_fkey_trigger_functions/blob/master/README.md'
+    );
+    execute _ddl_cmd_to_set_pg_role_fkey_trigger_functions_url;
+exception
+    when insufficient_privilege then
+        -- We say `superuser = false` in the control file; so let's just whine a little instead of crashing.
+        raise warning using
+            message = format(
+                'Because you''re installing the pg_role_fkey_trigger_functions extension as non-superuser'
+                ' and because you are also not the owner of the %I DB, the database-level'
+                ' `pg_role_fkey_trigger_functions.readme_url` setting has not been set.',
+                current_database()
+            )
+            ,detail = 'Settings of the form `<extension_name>.readme_url` are used by `pg_readme` to'
+                || ' cross-link between extensions their README files.'
+            ,hint = 'If you want full inter-extension README cross-linking, you can ask your friendly'
+                || E' neighbourhood DBA to execute the following statement:\n'
+                || _ddl_cmd_to_set_pg_role_fkey_trigger_functions_url || ';';
 end;
 $$;
 
